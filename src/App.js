fd96657a6e57a698501f23870d6be34a5561dafe
Kuivalainen <d.kuivalainen@yandex.ru>
2022-01-31 17:48:22 +0200

    Add testnet description

--- src/App.js
+++ src/App.js
@@ -5,7 +5,12 @@ import useLogin from "./utils/useLogin";
 function App() {
   const { signIn, isSigned, loading } = useLogin();
 
-  if (!isSigned) return <button onClick={signIn}>SIGN IN</button>;
+  if (!isSigned)
+    return (
+      <div>
+        Sign in with Ropsten testnet <button onClick={signIn}>SIGN IN</button>
+      </div>
+    );
   if (loading) return <p>loading...</p>;
   return <Tokens key={uid()} />;
 }
